2d6c6e1d68f8b793099cd89fee862cf4aed328c3
David <david@purview.io>
2020-06-21 20:30:52 -0700

    add server

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require('mongoose');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+mongoose.connect("mongodb+srv://admin-david:9yciX!q8w@7k96D@cluster0-fhnuq.mongodb.net/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function (req, res) {
+    Article.find(function (err, foundArticles) {
+        res.send(foundArticles);
+        console.log('Articles:', foundArticles)
+    });
+});
+
+app.listen(port, error => {
+    if (error) throw error;
+    console.log('Server running on port ' + port);
+});
+
+app.post('/payment', (req, res) => {
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: 'usd'
+    };
+
+    stripe.charges.create(body, (stripeErr, stripeRes) => {
+        if (stripeErr) {
+            res.status(500).send({ error: stripeErr });
+        } else {
+            res.status(200).send({ success: stripeRes });
+        }
+    });
+});
